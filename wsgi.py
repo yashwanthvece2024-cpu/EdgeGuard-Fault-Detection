5e5d1cad42e206ac69564f41fb21ff63bb32f767
Yashwanth.V <yashwanthv.ece2024@citchennai.net>
2026-09-18 20:08:34 +0530

    Update wsgi.py with explicit sys.path injection

--- wsgi.py
+++ wsgi.py
@@ -1,1 +1,8 @@
-from src.api.main import app
+import sys
+import os
+
+# Explicitly add root and src directories to Python path
+sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
+sys.path.insert(0, os.path.join(os.path.abspath(os.path.dirname(__file__)), "src"))
+
+from api.main import app
